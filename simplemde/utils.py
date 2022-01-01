33f7f558183e8e228b4def389903932978b822a6
Vadim Galaktionov <vadim.galaktionov@gmail.com>
2022-01-01 15:30:05 +0100

    Update utils.py

--- simplemde/utils.py
+++ simplemde/utils.py
@@ -4,7 +4,7 @@ from importlib import import_module
 try:
     from django.utils.encoding import force_str
 except ImportError:
-    from django.utils.encoding import force_unicode as force_text
+    from django.utils.encoding import force_unicode as force_str
 from django.utils.functional import Promise
 
 import json
